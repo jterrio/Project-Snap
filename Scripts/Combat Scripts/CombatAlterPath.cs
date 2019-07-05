febf341d32cb8df457e86c88867cc65969daf8ac
jterrio <31709118+jterrio@users.noreply.github.com>
2019-07-05 13:36:36 -0500

    Created CombatAlterPath
    -Check for OnMouseDrag and calls functions in CombatHudLog

---
+++ Scripts/Combat Scripts/CombatAlterPath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatAlterPath : MonoBehaviour{
+
+    public void OnMouseDrag() {
+        if (!UIManager.ins.IsSelectingInCombat()) {
+            Vector3 m = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            m = new Vector3(m.x, m.y, 0);
+            CombatManager.ins.combatHUDLog.ChangeNode(transform.position, m);
+        }
+    }
+
+}
